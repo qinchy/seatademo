a4a26f880d487fc0fd75fabd4bad2ee7c9fa1bea
qinchaoyong <chaoyong.qin@nascent.cn>
2020-12-15 18:19:39 +0800

    更新代码

--- seatademo-start/src/main/java/com/qinchy/seatademo/seata/SeatademoApplication.java
+++ seatademo-start/src/main/java/com/qinchy/seatademo/seata/SeatademoApplication.java
@@ -13,8 +13,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  **/
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 @RefreshScope
+@EnableFeignClients
 public class SeatademoApplication {
 
     public static void main(String[] args) {
